119411c8c74fab4e4fe571aba3b9dc58dd291c2f
hasan <hasanakoz1986@gmail.com>
2022-09-24 14:21:07 +0300

    countries fetch api

--- flag-app/app.js
+++ flag-app/app.js
@@ -1,3 +1,62 @@
 //*=========================================================
 //*                     FLAG-APP
 //*=========================================================
+
+const fetchCountryByName = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        renderError(`something went wrong: ${res.status}`);
+        throw new Error();
+      }
+      return res.json();
+    })
+    .then((data) => renderCountries(data))
+    .catch((err) => console.log(err));
+
+  const renderError = () => {
+    const countryDiv = document.querySelector(".countries");
+    countryDiv.innerHTML += `
+    <h2>Countries cannot be fetched</h2>
+    <img src = "./img/404.png"/>`;
+  };
+
+  const renderCountries = (data) => {
+    const countryDiv = document.querySelector(".countries");
+    console.log(data);
+    const {
+      capital,
+      currencies,
+      flags: { svg },
+      languages,
+      name: { common },
+      region,
+    } = data[0];
+    countryDiv.innerHTML += `
+    <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
+  <img src="${svg}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${common}</h5>
+    <p class="card-text">${region}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"><i class = "fas fa-lg fa-landmark"></i>${capital}</li>
+    <li class="list-group-item"><i class="fa-solid fa-comments"></i>${Object.values(
+      languages
+    )}
+
+    </li>
+    <li class="list-group-item">
+    <i class="fas fa-lg fa-money-bill-wave"></i> ${
+      Object.values(currencies)[0].name
+    }${Object.values(currencies)[0].symbol}
+</li>
+  </ul>
+  </div>`;
+  };
+};
+fetchCountryByName("turkey");
+
+fetchCountryByName("western sahara");
+fetchCountryByName("South Africa");
